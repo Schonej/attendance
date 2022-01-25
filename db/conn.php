c68912c46b35f7cbbd96fa42d2f790506a5d456d
Your Name <you@example.com>
2022-01-24 21:48:15 -0700

    commit

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,16 @@
 <?php 
-    $host = '127.1.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $password = '';
+    //development connection
+    // $host = '127.1.0.1';
+    // $db = 'attendance_db';
+    // $user = 'root';
+    // $password = '';
+    // $charset = 'utf8mb4';
+
+    //Remote database connection
+    $host = 'sql3.freesqldatabase.com';
+    $db = 'sql3467875';
+    $user = 'sql3467875';
+    $password = 'yVBklA71ZQ';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
